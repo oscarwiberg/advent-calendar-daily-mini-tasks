857cf2477b2dc9d2d1d53182990b35e90dbbce78
Oscar Wiberg <wgc948@c02fc0cyml7j.lan>
2022-12-09 16:15:47 +0100

    first commit

---
+++ 5/index.js
@@ -0,0 +1,29 @@
+import products from "./data.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+function getSaleItems(data){
+    const sweets = data.filter(sweet => sweet.type === "sweet" )
+    const result = sweets.map(sweet => {
+        delete sweet.type
+        return sweet
+    })
+    return result
+};
+
+console.log(getSaleItems(products))
---
+++ 8/index.js
@@ -0,0 +1,58 @@
+// This test is to evaluate if the timestamp is a valid timestamp
+
+
+function validTime(str) {
+    //  write code here.
+    const splitTime = str.split(":")
+    const hours = parseInt(splitTime[0])
+    const minutes = parseInt(splitTime[1])
+    
+    return hours >= 24 || minutes >=59 ? false : true
+}
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
