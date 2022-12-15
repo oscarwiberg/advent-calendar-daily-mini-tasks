c41e871f0833aaea139e45e777a3e705551b85b4
Oscar Wiberg <oscar.wiberg@live.se>
2022-12-15 11:40:03 +0100

    update

---
+++ 10/index.js
@@ -0,0 +1,26 @@
+function sortByLength(strs) {
+    const sortedArr = strs.sort(function(a, b) {
+        return a.length - b.length
+    })
+
+    return sortedArr;
+}
+
+/**
+* Test Suite 
+*/
+describe('sortByLength()', () => {
+    it('sorts the strings from shortest to longest', () => {
+        // arrange
+        const strs = ["abc", "", "aaa", "a", "zz"];
+        
+        // act
+        const result = sortByLength(strs);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+    });
+});
